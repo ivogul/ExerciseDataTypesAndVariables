5481ced0d7fdbadfbd7602e1fd927999ee33d0d4
ivogul <ivogul@gmail.com>
2017-10-01 21:58:05 +0300

    update to ex 6

---
+++ DataTypesAndVariablesEx/ExchangeVariableValues/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExchangeVariableValues
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 5;
+            int b = 10;
+            Console.WriteLine($"Before: \r\na = {a}\r\nb = {b}");
+            int temp = a;
+            a = b;
+            b = temp;
+            Console.WriteLine($"After: \r\na = {a}\r\nb = {b}");
+        }
+    }
+}
